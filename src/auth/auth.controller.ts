f7d2904da7d8018a38fd661e551580bc2883c4d5
sanyam03 <sanyambharani01@gmail.com>
2022-02-09 18:33:55 +0530

    Adding Auth folder from Authentication branch

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('login')
+  @UseGuards(AuthGuard('google'))
+  googleAuth(@Req() req) {}
+
+  @Get('redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.authService.googleLogin(req);
+  }
+}
